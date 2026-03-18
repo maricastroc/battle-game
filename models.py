cd8b59656bf8627412702ca950d8d9f0743fd0af
mariana castro <marianacastrorc@gmail.com>
2026-03-18 16:32:58 -0300

    first commit

---
+++ models.py
@@ -0,0 +1,102 @@
+class Character:
+  def __init__(self, name: str, level: int, life_points: int, mana: int):
+    self._name = name
+    self._level = level
+    self._life_points = life_points
+    self._mana = mana
+
+  @property
+  def name(self) -> str:
+    return self._name
+  
+  @property
+  def level(self) -> int:
+    return self._level
+  
+  @property
+  def life_points(self) -> int:
+    return self._life_points
+  
+  @property
+  def mana(self) -> int:
+    return self._mana
+  
+  @mana.setter
+  def mana(self, value: int) -> None:
+      if value < 0:
+        self._mana = 0
+      else:
+        self._mana = value
+
+  @life_points.setter
+  def life_points(self, value: int) -> None:
+      if value < 0:
+        self._life_points = 0
+      else:
+        self._life_points = value
+  
+  def is_alive(self) -> bool:
+    return self.life_points > 0
+  
+  def take_damage(self, amount: int) -> None:
+    self.life_points -= amount
+
+class Ability:
+  def __init__(self, name: str, damage: int, mana_cost: int):
+    self._name = name
+    self._damage = damage
+    self._mana_cost = mana_cost
+
+  @property
+  def name(self) -> str:
+    return self._name
+  
+  @property
+  def damage(self) -> int:
+    return self._damage
+  
+  @property
+  def mana_cost(self) -> int:
+    return self._mana_cost
+
+  def use(self, caster: Character, target: Character) -> None:
+    if caster.mana < self.mana_cost:
+      print('Not enough mana.')
+      return
+    
+    caster.mana -= self.mana_cost
+    target.take_damage(self.damage)
+    
+    print(f"{caster.name} used {self.name} on {target.name}, dealing {self.damage} damage!")
+
+    if not target.is_alive():
+      print(f"Game over! {caster.name} wins!")
+
+class Hero(Character): 
+    def __init__(self, name: str, level: int, life_points: int, mana: int, ability: Ability):
+      super().__init__(name, level, life_points, mana)
+      self._ability = ability
+
+    def attack(self, target: Character) -> None:
+      self._ability.use(self, target)
+
+class Enemy(Character): 
+    def __init__(self, name: str, level: int, life_points: int, mana: int, enemy_type: str, damage: int):
+      super().__init__(name, level, life_points, mana)
+      self._enemy_type = enemy_type
+      self._damage = damage
+
+    @property
+    def enemy_type(self) -> str:
+      return self._enemy_type
+    
+    @property
+    def damage(self) -> int:
+      return self._damage
+    
+    def attack(self, target: Character) -> None:
+      target.take_damage(self.damage)
+      print(f"{self.name} attacked {target.name}, dealing {self.damage} damage!")
+
+      if not target.is_alive():
+        print(f"Game over! {self.name} wins!")
